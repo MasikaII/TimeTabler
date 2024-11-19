cc3db49898b12089b4f69c110a082b99976644ae
J.KINUTHIA <73335089+seph-kinuthia@users.noreply.github.com>
2024-11-19 15:39:26 +0300

    first commit

---
+++ src/main/java/com/api/timetabler/TimetablerApplication.java
@@ -0,0 +1,13 @@
+package com.api.timetabler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetablerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetablerApplication.class, args);
+	}
+
+}
